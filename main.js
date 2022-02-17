8678921319f6dbd4adb0aa4dbeab4e8bbb1d9ee3
JethroSama <kel73896@gmail.com>
2022-02-17 17:13:48 +0800

    implement cleanup and add copy to clipboard function

--- main.js
+++ main.js
@@ -24,12 +24,26 @@ function cleanup(text) {
     return replaceDot
 }
 
+function copyToClipboard(text) {
+    const elem = document.createElement('textarea');
+    elem.value = text;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+}
+
 function mobile() {
     const content = document.querySelector('.bookContent').textContent
+
     const title = document.querySelector('h3')
     const copyButton = document.createElement("button")
     copyButton.style = 'padding: 5px;'
     copyButton.innerText = 'Copy'
+    copyButton.onclick = function () {
+        const clean = cleanup(content)
+        console.log(clean)
+    }
     title.appendChild(copyButton)
 
 }
