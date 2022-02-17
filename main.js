0580b5e35cbceb75bbd7a8245e10f8492e896223
JethroSama <kel73896@gmail.com>
2022-02-17 17:08:51 +0800

    add cleanup function

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ if (url.indexOf('https://sj.uukanshu.com/') === 0) {
     console.log('not mobile')
 }
 
+function cleanup(text) {
+
+    const textBasic = text.replace(
+        /[\uff01-\uff5e]/g,
+        function (ch) { return String.fromCharCode(ch.charCodeAt(0) - 0xfee0); }
+    )
+
+    const removedUseless = textBasic.replaceAll('UU看书 www.uukanshu.com', '')
+    const removedUseless1 = removedUseless.replaceAll('UU看書 www.uukanshu.com', '')
+    const removedUseless2 = removedUseless1.replaceAll('UU看書www.uukanshu.com', '')
+    const removedUseless3 = removedUseless2.replaceAll('UU看书www.uukanshu.com', '')
+    const addSpace = removedUseless3.replaceAll(',', ', ')
+    const addSpace1 = addSpace.replaceAll(':', ': ')
+    const replaceDot = addSpace1.replaceAll('。', '.')
+    return replaceDot
+}
 
 function mobile() {
     const content = document.querySelector('.bookContent').textContent
@@ -15,5 +31,5 @@ function mobile() {
     copyButton.style = 'padding: 5px;'
     copyButton.innerText = 'Copy'
     title.appendChild(copyButton)
-    console.log(title.textContent)
+
 }
