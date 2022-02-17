0eada750cc734d1618ae908039cef2cf70c8de7a
JethroSama <kel73896@gmail.com>
2022-02-17 16:59:40 +0800

    check mobile and desktop

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert("it works")
+const url = window.location.href
+// check if base url matches https://sj.uukanshu.com/
+if (url.indexOf('https://sj.uukanshu.com/') === 0) {
+    console.log('mobile')
+    mobile()
+} else {
+    console.log('not mobile')
+}
+
+
+function mobile() {
+    const content = document.querySelector('.bookContent')
+    console.log(content.textContent)
+}
